5976eef39c05b4c9677c9c178fcff88f0febafc5
dangermouse124 <pimail124@gmail.com>
2019-01-19 15:24:18 +0800

    Added user authentication and registration

--- basecamp/urls.py
+++ basecamp/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from mqtt.views import Register
 
 urlpatterns = [
+    path('register/success/', TemplateView.as_view(template_name="registration/success.html"), name='register-success'),
+    path('resiter/', Register.as_view(), name='register'),
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
     path('', include('mqtt.urls')),
 ]
--- mqtt/views.py
+++ mqtt/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url=reverse_lazy('login'))
 def index(request):
     #return HttpResponse("<h1>Basecamp Homepage</h1>")
     return render (request, 'mqtt/mqtt.html')
+
+class Register(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
