c67eae63575f18bebe1cbb1d0fc18c9116a370a2
dangermouse124 <pimail124@gmail.com>
2019-01-21 08:09:27 +0800

    Added model for temperature

--- mqtt/models.py
+++ mqtt/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class temp_10min(models.Model):
+
+    sensor_id = models.models.IntegerField()
+    date_time = models.DateTimeField(auto_now_add=True)
+    temperature = models.FloatField()
+
+    def __str__(self):
+        return self.Sensor_id
+
+class temp_max_min(models.Model):
+
+    sensor_id = models.models.IntegerField()
+    date = models.DateField()
+    max = models.IntegerField()
+    max_time = models.TimeField()
+    min = models.IntegerField()
+    min_time = models.TimeField()
+
+    def __str__(self):
+        return self.sensor_id
+
